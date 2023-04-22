6dec57d740d4c0d793071169b5eec0f2d03d5175
Neil <z5202dbc@gmail.com>
2023-04-22 14:54:24 +0800

    重構。添加GUI。

--- CIR_downloader.py
+++ CIR_downloader.py
@@ -1,180 +1,45 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
-from datetime import datetime, timedelta, timezone
 import logging
-import time, os
+import time, os, sys, signal
+import tkinter as tk
+from download import Downloader
+from loggy import setup_logging
 
 
 
-def setup_logging(logDir):
-    dt1 = datetime.utcnow().replace(tzinfo=timezone.utc)
-    now = dt1.astimezone(timezone(timedelta(hours=8))) # 轉換時區 -> 東八區
-    date_today = datetime.strftime(now, '%Y-%m-%d')
-    
-    logName = date_today + '.log'
-    # logDir  = 'log'
-    logPath = 'log/' + logDir + '/' + logName
-
-    os.makedirs('log/' + logDir,exist_ok=True)
-
-    allLogger = logging.getLogger(logDir+'allLogger')
-    allLogger.setLevel(logging.DEBUG)
-
-    fileHandler = logging.FileHandler(logPath, encoding='utf-8',mode='a')
-    fileHandler.setLevel(logging.INFO)
-
-    streamHandler = logging.StreamHandler()
-    streamHandler.setLevel(logging.WARNING)
-
-    AllFormatter = logging.Formatter("%(asctime)s - [line:%(lineno)d] - %(levelname)s: %(message)s")
-    fileHandler.setFormatter(AllFormatter)
-    streamHandler.setFormatter(AllFormatter)
-
-    allLogger.addHandler(fileHandler)
-    allLogger.addHandler(streamHandler)
-
-    return allLogger
+def check_main_path():
+    if not os.path.isdir(main_path):
+        os.makedirs(main_path)
 
 
 
+def download(path, logger):
 
-# logging
-logDir = 'crawler'
-loggerName = logDir+'allLogger'
-setup_logging(logDir)
-logger = logging.getLogger(loggerName)
+    global main_path
+    main_path = path
+    check_main_path()
 
+    D = Downloader(main_path, logger)
 
+    # web driver init
+    chrome_browser = D.initialize_parser()
 
+    # Ctrl+C 終止程序
+    signal.signal(signal.SIGINT, signal_handler)
 
-
-
-def initialize_parser():
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option('detach', True) # 控制瀏覽器自動關閉
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-
-    options.add_experimental_option('prefs', {
-        "download.default_directory": r"C:\Users\z5202\training\parse-CIR\pdf",
-        "download.prompt_for_download": False,
-        "download.directory_upgrade": True,
-        "plugins.always_open_pdf_externally": True
-    })
-
-    chrome_browser = webdriver.Chrome('./chromedriver', options=options)
-    chrome_browser.maximize_window()
-    chrome_browser.get('https://www.cir-safety.org/ingredients') # CIR - INGREDIENTS 網頁
-    return chrome_browser
-    
-
-
-def open_alphabet_tabs(chrome_browser, alphabets):
-    childs_window = {}
-    count = 0
-    for alpha in alphabets:
-        if count > 2:
-            break
-
-        windows_old = chrome_browser.window_handles
-
-        button = alpha.find_element(By.TAG_NAME, 'a')
-        time.sleep(1.5)
-        button.click()
-        count += 1
-        print(f'Finish opening {alpha.text}')
-
-        # matching opened windows
-        windows_new = chrome_browser.window_handles
-        new = list(set(windows_new) - set(windows_old))
-        childs_window[alpha.text] = new[0]
-    return childs_window
-
-
-def fetch_target_table(chrome_browser, try_times = 3, fail_times = 0):
-    # 設置等待時間
-    wait = WebDriverWait(chrome_browser, 10)
-
-    target_table = False
-    location = (By.XPATH, '/html/body/form/table/tbody/tr[2]/td/table/tbody')
-
-    while fail_times < try_times and target_table is False:
-        try:
-            target_table = wait.until(EC.presence_of_element_located(location))
-        except Exception as e:
-            fail_times += 1
-            print('\nFail times:', fail_times)
-            print(str(e))
-            logger.error(str(e))
-
-        
-    return target_table
-
-
-def downloader(alphabet, target, index):
-    
-    # print('\ntarget:', target.get_attribute('href'))
-    file_name = target.get_attribute('href').split('.pdf')[0].split('/')[-1]
-    print(f'\nfilename =>   [ {file_name} ]')
-
-    destination_directory = f'pdf\\{alphabet}'
-    if not os.path.isdir(destination_directory):
-        os.makedirs(destination_directory)
-
-    path = f'{destination_directory}\\{file_name}.pdf'
-    if not os.path.isfile(path):
-        print("\nIt's a new file. Start to Download it.")
-
-        target.click()
-    
-        finish = False
-        t = 0
-        timeout = 10
-        while finish is not True and t < timeout:
-            finish = False
-            t += 1
-            print('not yet..')
-            time.sleep(1)
-            if os.path.isfile(f'pdf\\{file_name}.pdf'):
-                finish = True
-                print('Download is finished')
-        try:
-            os.replace(f'pdf\\{file_name}.pdf', path)
-        except Exception as e:
-            print(e)
-            logger.error(f'alphabet => {alphabet}, ingredient => {index},  Replace failed.')
-
-        print('\nFile added !')
-
-    else:
-        print("\nExists already")
-
-
-
-
-# web driver init
-chrome_browser = initialize_parser()
-
-# Action chains init
-actions = ActionChains(chrome_browser)
-
-
-def main():
     alphabets_div = chrome_browser.find_element(By.CLASS_NAME, 'glossary ')
     alphabets = alphabets_div.find_elements(By.CLASS_NAME, 'glossary-letter')
 
-    # CIR 主頁面
-    parent_window = chrome_browser.current_window_handle
+    # 打開字母分頁
+    childs_window = D.open_alphabet_tabs(chrome_browser, alphabets)
 
-
-    # 將字母分頁全部開好
-    childs_window = open_alphabet_tabs(chrome_browser, alphabets)
-
+    # TODO args[2] - alphabet
 
     # 照字母順序A-Z
-    for alphabet in childs_window.keys():
+    for alphabet in childs_window.keys(): 
 
         chrome_browser.switch_to.window(childs_window[alphabet])
         print(f"\nSwitch to Alphabet [{alphabet}]")
@@ -189,10 +54,11 @@ def main():
         except Exception as e:
             print(str(e))
 
-
         time.sleep(2)
 
-        download_list = choose_file_to_download(alphabet, buttons, childs_window)
+        # TODO args[3] - button
+
+        download_list = choose_file_to_download(chrome_browser, D, alphabet, buttons, childs_window)
 
         result_directory = 'result'
         if not os.path.isdir(result_directory):
@@ -204,22 +70,18 @@ def main():
                 f.write(ingredient+'\n')
         
         
-        
 
-
-def choose_file_to_download(alphabet, buttons, childs_window):
+def choose_file_to_download(chrome_browser, D, alphabet, buttons, childs_window):
     # list
     through = list()
     index = 0
 
+    # buttons = buttons[100:]        # 測試用
 
-
-    # buttons = buttons[:]        # 測試用
+    actions = ActionChains(chrome_browser)
 
     for button in buttons:
         
-        
-
         # 定位項目 item
         print('\n*************  ', button.text, ' *************\n')
         time.sleep(1)
@@ -227,11 +89,9 @@ def choose_file_to_download(alphabet, buttons, childs_window):
         # 重置滑鼠
         actions.reset_actions()
 
-
         # 點擊item(人名)，打開pdf選擇視窗
         actions.move_to_element(button).click()
         actions.perform()
-
 
         time.sleep(1)
 
@@ -239,24 +99,21 @@ def choose_file_to_download(alphabet, buttons, childs_window):
         chrome_browser.switch_to.window(chrome_browser.window_handles[-1])
         print("Switch to pdf page: " + str(chrome_browser.current_url) + '\n')
 
-        target_table = fetch_target_table(chrome_browser)
+        target_table = D.fetch_target_table(chrome_browser)
         if target_table is False:
-            
             print('\nPDF表格獲取失敗')
             print('結束爬蟲')
-            logger.error(f'alphabet => {alphabet}, index => {index},  [ fetch_target_table ] failed.')
+            D.logger.error(f'alphabet => {alphabet}, index => {index},  [ fetch_target_table ] failed.')
             return
             
         target_trs   = target_table.find_elements(By.XPATH, './tr')
-
         del(target_trs[0])
-
         
-        # download = False
         latest_year = 1500
         tr_index = 1
-        escape_status_string = ['Insufficient', 'not opened', 'Re-evaluation', 'No Reported Uses']
-        accept_status_string = ['Published Report', 'Final Report']
+        escape_status_string = ['Insufficient', 'not opened', 'Re-evaluation', 'No Reported Uses', 'Use Not Supported']
+        accept_status_string = ['Published Report', 'Final Report', 'Scientific Literature Review']
+        accept = []
         for tr in target_trs:
             time.sleep(1)
             
@@ -269,13 +126,18 @@ def choose_file_to_download(alphabet, buttons, childs_window):
             date       = tds[2].find_element(By.XPATH, './font')
 
             if ingredient.text not in through:
+                if len(through) == 0:
+                    global first
+                    first = ingredient.text
                 through.append(ingredient.text)
+                global last
+                last = ingredient.text
 
-            # if status.text not in accept_status_string:
             if status.text in escape_status_string:
-                logger.warning(f'alphabet => {alphabet}, index => {index}, tr_index => {tr_index}')
-                logger.warning(f'{status.text},  [ accept_status_string ] failed.')
                 continue
+            else:
+                if status.text not in accept_status_string:
+                    accept.append(status.text)
 
             if len(date.text) > 0:
                 print(f'date_{tr_index} => ', date.text)
@@ -297,18 +159,24 @@ def choose_file_to_download(alphabet, buttons, childs_window):
                     target = status
             tr_index += 1
 
+        
+        # New Status Found
+        if len(accept) > 0:
+            D.logger.warning(f'  alphabet => {alphabet}, index => {index}, tr_index => {tr_index}')
+            D.logger.warning(f'  [ New Status ]  =>  {status.text}')
+
             
         print('latest_year: ',latest_year)
             
         time.sleep(1)
 
-        downloader(alphabet, target, index)
+        D.downloader(alphabet, target, index)
 
         time.sleep(1)
 
 
         print('\nnums: ',len(through))
-        print(through)
+        # print(through)
 
         index += 1
 
@@ -322,6 +190,46 @@ def choose_file_to_download(alphabet, buttons, childs_window):
 
 
 
+
+def signal_handler(self, signal, frame):
+    print('\n\n------------- 您終止了爬蟲程序 -------------')
+    print('\n此次爬蟲...\n')
+    print('　第一筆資料為：　　',first)
+    print('最後一筆資料為：　　',last)
+    sys.exit(0)
+
+
+
+
+def main():
+    # logging
+    logDir = 'crawler'
+    loggerName = logDir+'allLogger'
+    setup_logging(logDir)
+    logger = logging.getLogger(loggerName)
+
+
+    window = tk.Tk()
+    window.title('CIR parser')
+    window.geometry("300x100")
+    # 標示文字
+    label = tk.Label(window, text = 'Download path:')
+    label.pack()
+
+    # 輸入欄位
+    entry = tk.Entry(window, # 輸入欄位所在視窗
+        width = 25) # 輸入欄位的寬度
+    entry.pack()
+
+    # 建立按鈕
+    entry_btn = tk.Button(window, # 按鈕所在視窗
+        text = 'Start',  # 顯示文字
+        command=lambda: download(entry.get(), logger)) # 按下按鈕所執行的函數
+    # entry_btn.place(x=130, y=50)
+    # 以預設方式排版按鈕
+    entry_btn.pack(side='top', pady=10)
+
+    window.mainloop()
 
 
 
---
+++ download.py
@@ -0,0 +1,112 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os, time
+
+class Downloader:
+
+    def __init__(self, main_path, logger) -> None:
+        self.main_path = main_path
+        self.logger    = logger
+    
+
+    def initialize_parser(self):
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('detach', True) # 控制瀏覽器自動關閉
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+        options.add_experimental_option('prefs', {
+            "download.default_directory": self.main_path,
+            "download.prompt_for_download": False,
+            "download.directory_upgrade": True,
+            "plugins.always_open_pdf_externally": True
+        })
+
+        chrome_browser = webdriver.Chrome('./chromedriver', options=options)
+        chrome_browser.maximize_window()
+        chrome_browser.get('https://www.cir-safety.org/ingredients') # CIR - INGREDIENTS 網頁
+        return chrome_browser
+
+
+
+    def open_alphabet_tabs(self, chrome_browser, alphabets):
+        childs_window = {}
+        count = 0
+        for alpha in alphabets:
+            if count > 2:
+                break
+
+            windows_old = chrome_browser.window_handles
+
+            button = alpha.find_element(By.TAG_NAME, 'a')
+            time.sleep(1.5)
+            button.click()
+            count += 1
+            print(f'Finish opening {alpha.text}')
+
+            # matching opened windows
+            windows_new = chrome_browser.window_handles
+            new = list(set(windows_new) - set(windows_old))
+            childs_window[alpha.text] = new[0]
+        return childs_window
+
+
+    def fetch_target_table(self, chrome_browser, try_times = 3, fail_times = 0):
+        # 設置等待時間
+        wait = WebDriverWait(chrome_browser, 10)
+        target_table = False
+        location = (By.XPATH, '/html/body/form/table/tbody/tr[2]/td/table/tbody')
+        while fail_times < try_times and target_table is False:
+            try:
+                target_table = wait.until(EC.presence_of_element_located(location))
+            except Exception as e:
+                fail_times += 1
+                print('\nFail times:', fail_times)
+                print(str(e))
+                self.logger.error(str(e))
+        return target_table
+
+    
+
+    def downloader(self, alphabet, target, index):
+
+        file_name = target.get_attribute('href').split('.pdf')[0].split('/')[-1]
+        print(f'\nfilename =>   [ {file_name}.pdf ]')
+
+        default_file_path     = f'{self.main_path}\\{file_name}.pdf'
+        destination_directory = f'{self.main_path}\\pdf\\{alphabet}'
+        file_path             = f'{destination_directory}\\{file_name}.pdf'
+
+        if not os.path.isdir(destination_directory):
+            os.makedirs(destination_directory)
+
+        if not os.path.isfile(file_path):
+            print("\nIt's a new file. Start to Download it.")
+            target.click()
+            finish = False
+            t = 0
+            timeout = 10
+            while finish is not True and t < timeout:
+                finish = False
+                t += 1
+                print('not yet..')
+                time.sleep(1)
+                if os.path.isfile(default_file_path):
+                    finish = True
+            try:
+                os.replace(default_file_path, file_path)
+                print('Download is finished')
+            except Exception as e:
+                print(e)
+                self.logger.error(f'alphabet => {alphabet}, ingredient => {index},  Replace failed.')
+            print('\nFile added !')
+        else:
+            print("\nExists already")
+
+
+
+    
+
+
+    
